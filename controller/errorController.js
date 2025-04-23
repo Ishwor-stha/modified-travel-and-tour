20e1a177fe64942b9550a1b82ccfd3f9497fbe14
ishwor-stha <ishwor.stha.bank@gmail.com>
2025-04-23 11:18:03 +0545

    logging the .env variable in errorController for checking

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,7 +1,7 @@
 module.exports=(err,req,res,next)=>{
     const statusCode = err.statusCode || 500;  // Default to 500 if statusCode is not set
     const status = err.status || "false";      // Default to "error" if status is not set
-
+    // console.log(process.env.NODE_ENV)
     // all the error details comes from the user through ../utils/error handling
     if(process.env.NODE_ENV=="development"){
         return res.status(statusCode).json({
