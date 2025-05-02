5e9a67019193013e070aedfe31e4fb55762e2b7a
ishwor-stha <ishwor.stha.bank@gmail.com>
2025-05-02 15:17:44 +0545

    importing the individual controller in tourRouter

--- route/tourRoute.js
+++ route/tourRoute.js
@@ -1,24 +1,24 @@
 const express = require("express");
 // const upload = require("../utils/multer.js");
 const Router = express.Router();
-const tourController = require("../controller/tourController.js");
+const {postTour,updateTour,deleteTour,getOneTour,bookTour,enquiry} = require("../controller/tourController.js");
 const { checkJwt } = require("../controller/authController.js");
 
 
 /*********************************Route****************************************************** */
 Router.route("/get-tours").get(tourController.getTours);
 
-Router.route("/tour-admin/post-tour").post(checkJwt, /*upload.array('image', 4),*/ tourController.postTour);
+Router.route("/tour-admin/post-tour").post(checkJwt, /*upload.array('image', 4),*/ postTour);
 
-Router.route("/tour-admin/update-tour/:id").patch(checkJwt, /*upload.array('image', 4),*/ tourController.updateTour);
+Router.route("/tour-admin/update-tour/:id").patch(checkJwt, /*upload.array('image', 4),*/ updateTour);
 
-Router.route("/tour-admin/delete-tour/:id").delete(checkJwt, tourController.deleteTour);
+Router.route("/tour-admin/delete-tour/:id").delete(checkJwt, deleteTour);
 
-Router.route("/get-tour/:slug").get(tourController.getOneTour);
+Router.route("/get-tour/:slug").get(getOneTour);
 
-Router.route("/book-tour").post(tourController.bookTour);
+Router.route("/book-tour").post(bookTour);
 
-Router.route("/enquiry").post(tourController.enquiry);
+Router.route("/enquiry").post(enquiry);
 
 
 module.exports = Router;
