34e6edb0c2dacbeb51922700a7937aea8d9d21fb
ishwor-stha <ishwor.stha.bank@gmail.com>
2025-05-05 13:57:06 +0545

    fixed error in torRoute

--- route/tourRoute.js
+++ route/tourRoute.js
@@ -1,12 +1,11 @@
 const express = require("express");
 // const upload = require("../utils/multer.js");
 const Router = express.Router();
-const {postTour,updateTour,deleteTour,getOneTour,bookTour,enquiry} = require("../controller/tourController.js");
+const {postTour,updateTour,deleteTour,getOneTour,bookTour,enquiry,getTours} = require("../controller/tourController.js");
 const { checkJwt } = require("../controller/authController.js");
 
-
 /*********************************Route****************************************************** */
-Router.route("/get-tours").get(tourController.getTours);
+Router.route("/get-tours").get(getTours);
 
 Router.route("/tour-admin/post-tour").post(checkJwt, /*upload.array('image', 4),*/ postTour);
 
