31335f4f250c9e2a918c13dd9ca943dd5995b18f
ishwor-stha <ishwor.stha.bank@gmail.com>
2025-05-23 14:33:12 +0545

    changes in code

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -60,31 +60,105 @@ module.exports.payWithEsewa = async (req, res, next) => {
 }
 
 
+// module.exports.paymentSucess = async (req, res, next) => {
+//     try {
+        
+
+//         if (!req.query.data) return next(new errorHandler("Server error.",500))
+//         const encodedData = req.query.data;
+
+//         const decodedData = JSON.parse(Buffer.from(encodedData, "base64").toString("utf-8"));
+
+
+//         const TotalAmt = decodedData["total_amount"].replace(/,/g, '')//removing the comma from the amount for hashing the message ie (5,000)=>(5000)
+//         const message = `transaction_code=${decodedData.transaction_code},status=${decodedData.status},total_amount=${TotalAmt},
+//         transaction_uuid=${decodedData.transaction_uuid},product_code=${process.env.PRODUCT_CODE},signed_field_names=${decodedData.signed_field_names}`;
+
+//         const hash = crypto.createHmac("sha256", process.env.SECRET_KEY).update(message).digest("base64");
+
+//         if (hash !== decodedData.signature) {
+//             return res.json({
+//                 data:req.query.data,
+//                 decodedData,
+                
+//             })
+//             return next(new errorHandler("Invalid signature.",  500))
+//         }
+
+//         const response = await axios.get(process.env.STATUS_CHECK, {
+//             headers: {
+//                 Accept: "application/json",
+//                 "Content-Type": "application/json"
+//             },
+//             params: {
+//                 product_code: process.env.PRODUCT_CODE,
+//                 total_amount: TotalAmt,
+//                 transaction_uuid: decodedData.transaction_uuid
+//             }
+//         });
+
+//         const { status, transaction_uuid, total_amount } = response.data;
+//         if (status !== "COMPLETE" || transaction_uuid !== decodedData.transaction_uuid || Number(total_amount) !== Number(TotalAmt)) {
+//             return next(new errorHandler("Invalid transaction details", 500))
+
+//         }
+//         const userData= req.session.bookingData
+//         const tourData= req.session.tourData
+//         if(!userData || !tourData)return next(new errorHandler("User data or booking data is missing.",400));
+//         //after verification store something to the database ie(payemnt details etc) in my example i wil simply send success html file 
+
+//         // console.log(response.data)
+//         const htmlMessageUser = bookingMessageUser({
+//             userData ,
+//             tourData,
+//             transaction_uuid: response.data.transaction_uuid,
+//             ref_id: response.data.ref_id,
+//             amount: response.data.total_amount,
+//             advancePayment: userData.advancePayment,
+//             laterPayment: userData.payLater,
+//             bookingDate: new Date().toLocaleDateString()
+//         });
+
+//         const htmlMessageAdmin = bookingMessageAdmin({
+//             userData,
+//             tourData,
+//             transaction_uuid: response.data.transaction_uuid,
+//             ref_id: response.data.ref_id,
+//             amount: response.data.total_amount,
+//             advancePayment: userData.advancePayment,
+//             laterPayment: userData.payLater,
+//             bookingDate: new Date().toLocaleDateString()
+//         });
+//         // message sent to user
+//        await sendMessage(res,userData.email,"Payment Details",htmlMessageUser);
+//        await sendMessage(res,process.env.NODEMAILER_USER,"Payment Details",htmlMessageAdmin);
+
+//         return res.status(200).json({
+//             status: true,
+//             message: "Success",
+//             tourData: req.session.tourData,
+//             userData: req.session.bookingData,
+//             transaction_details: {
+//                 status: response.data.status,
+//                 ref_id: response.data.ref_id,
+//                 amount: response.data.total_amount
+
+//             }
+//         });
+//     } catch (error) {
+//         return next(new errorHandler(error.message, error.statusCode || 500))
+//     }
+// }
+
+
 module.exports.paymentSucess = async (req, res, next) => {
     try {
         
 
         if (!req.query.data) return next(new errorHandler("Server error.",500))
         const encodedData = req.query.data;
-        console.log("encodedData"+encodedData)
 
         const decodedData = JSON.parse(Buffer.from(encodedData, "base64").toString("utf-8"));
-                console.log("decoded "+decodedData)
-
-        const TotalAmt = decodedData["total_amount"].replace(/,/g, '')//removing the comma from the amount for hashing the message ie (5,000)=>(5000)
-        const message = `transaction_code=${decodedData.transaction_code},status=${decodedData.status},total_amount=${TotalAmt},
-        transaction_uuid=${decodedData.transaction_uuid},product_code=${process.env.PRODUCT_CODE},signed_field_names=${decodedData.signed_field_names}`;
-
-        const hash = crypto.createHmac("sha256", process.env.SECRET_KEY).update(message).digest("base64");
-
-        if (hash !== decodedData.signature) {
-            return res.json({
-                data:req.query.data,
-                decodedData,
-                
-            })
-            return next(new errorHandler("Invalid signature.",  500))
-        }
 
         const response = await axios.get(process.env.STATUS_CHECK, {
             headers: {
@@ -98,11 +172,7 @@ module.exports.paymentSucess = async (req, res, next) => {
             }
         });
 
-        const { status, transaction_uuid, total_amount } = response.data;
-        if (status !== "COMPLETE" || transaction_uuid !== decodedData.transaction_uuid || Number(total_amount) !== Number(TotalAmt)) {
-            return next(new errorHandler("Invalid transaction details", 500))
-
-        }
+       
         const userData= req.session.bookingData
         const tourData= req.session.tourData
         if(!userData || !tourData)return next(new errorHandler("User data or booking data is missing.",400));
@@ -133,19 +203,23 @@ module.exports.paymentSucess = async (req, res, next) => {
         // message sent to user
        await sendMessage(res,userData.email,"Payment Details",htmlMessageUser);
        await sendMessage(res,process.env.NODEMAILER_USER,"Payment Details",htmlMessageAdmin);
+       res.status().json({
+        status:true,
+        message:"Payment sucessfull"
+       })
 
-        return res.status(200).json({
-            status: true,
-            message: "Success",
-            tourData: req.session.tourData,
-            userData: req.session.bookingData,
-            transaction_details: {
-                status: response.data.status,
-                ref_id: response.data.ref_id,
-                amount: response.data.total_amount
+        // return res.status(200).json({
+        //     status: true,
+        //     message: "Success",
+        //     tourData: req.session.tourData,
+        //     userData: req.session.bookingData,
+        //     transaction_details: {
+        //         status: response.data.status,
+        //         ref_id: response.data.ref_id,
+        //         amount: response.data.total_amount
 
-            }
-        });
+        //     }
+        // });
     } catch (error) {
         return next(new errorHandler(error.message, error.statusCode || 500))
     }
